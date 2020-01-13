ba6d016a606ba01e44fd6f841f62ccb885554d96
csplinter <chris.splinter@datastax.com>
2020-01-13 12:03:42 -0600

    add ability to collect commits and contributors

--- GithubStats.py
+++ GithubStats.py
@@ -1,11 +1,10 @@
 import argparse
 import csv
+import os
 from datetime import datetime, timedelta
 from github import Github
 from github.GithubException import GithubException
 
-
-CSV_NAME = 'github-stats-' + datetime.now().strftime("%Y-%m-%d") + '.csv'
 DATES = []
 
 
@@ -21,6 +20,14 @@ def set_dates_for_week(start):
 def get_views(g, repo):
     repo = g.get_repo(repo)
     print(repo.full_name)
+
+    csv_name = 'github-stats-views-' + repo.full_name.split('/')[0] + '-' + datetime.now().strftime("%Y-%m-%d") + '.csv'
+
+    if not os.path.exists(csv_name):
+        with open(csv_name, mode='w') as output:
+            fields = ['Repository'] + DATES + ['Week Total']
+            writer = csv.writer(output, delimiter=',')
+            writer.writerow(fields)
 
     csv_row = [repo.full_name]
 
@@ -47,7 +54,7 @@ def get_views(g, repo):
         print()
         csv_row.append(weekly_view_count)
 
-        with open(CSV_NAME, mode='a') as output:
+        with open(csv_name, mode='a') as output:
             writer = csv.writer(output, delimiter=',')
             writer.writerow(csv_row)
 
@@ -56,9 +63,50 @@ def get_views(g, repo):
         print()
 
 
+def get_commits(g, repo, start, end):
+    repo = g.get_repo(repo)
+
+    csv_name = 'github-stats-commits-' + repo.full_name.replace('/', '-') + '-' + datetime.now().strftime("%Y-%m-%d") + '.csv'
+
+    with open(csv_name, mode='w') as output:
+        fields = ['Commit Date', 'Committer', 'Commit']
+        writer = csv.writer(output, delimiter=',')
+        writer.writerow(fields)
+
+    commit_url = 'https://github.com/{orgrepo}/commit/'.format(orgrepo=repo.full_name)
+    commits = repo.get_commits(since=start, until=end)
+    with open(csv_name, mode='a') as output:
+        for c in commits:
+            csv_row = list()
+            csv_row.append(c.commit.committer.date.strftime("%Y-%m-%d"))
+            csv_row.append(c.commit.committer.name)
+            csv_row.append(commit_url + c.sha[:7])
+
+            writer = csv.writer(output, delimiter=',')
+            writer.writerow(csv_row)
+
+
+def get_contributors(g, repo):
+    repo = g.get_repo(repo)
+    contributors = repo.get_contributors()
+
+    csv_name = 'github-stats-contributors-' + repo.full_name.replace('/', '-') + '-' + datetime.now().strftime(
+        "%Y-%m-%d") + '.csv'
+
+    if not os.path.exists(csv_name):
+        with open(csv_name, mode='w') as output:
+            fields = ['Date', 'Repository', 'Number of Contributors']
+            writer = csv.writer(output, delimiter=',')
+            writer.writerow(fields)
+
+    with open(csv_name, mode='a') as output:
+        writer = csv.writer(output, delimiter=',')
+        writer.writerow([datetime.now().strftime("%Y-%m-%d"), repo.full_name, contributors.totalCount])
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--all', default='all', action='store')
+    parser.add_argument('-m', '--mode', default='mode', action='store')
     parser.add_argument('-o', '--github_org', action='store')
     parser.add_argument('-r', '--github_repo', action='store')
     parser.add_argument('-t', '--github_access_token', action='store')
@@ -68,20 +116,20 @@ def main():
 
     g = Github(args.github_access_token)
 
-    set_dates_for_week(args.start_date)
+    if args.mode != 'contributors':
+        set_dates_for_week(args.start_date)
 
-    with open(CSV_NAME, mode='w') as output:
-        fields = ['Repository'] + DATES + ['Week Total']
-        writer = csv.writer(output, delimiter=',')
-        writer.writerow(fields)
-
-    if args.all == "all":
+    if args.mode == "views":
         org = g.search_repositories(query='org:{o} is:public'.format(o=args.github_org))
 
         for r in org:
             get_views(g, r.full_name)
-    else:
-        get_views(g, args.github_org + "/" + args.github_repo)
+    elif args.mode == "commits":
+        get_commits(g, args.github_org + '/' + args.github_repo,
+                    datetime.strptime(args.start_date, '%Y-%m-%d'),
+                    datetime.strptime(DATES[6], '%Y-%m-%d'))
+    elif args.mode == 'contributors':
+        get_contributors(g, args.github_org + '/' + args.github_repo)
 
 
 if __name__ == "__main__":
